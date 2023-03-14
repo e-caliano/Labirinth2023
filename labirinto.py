3300edbf729e7f2674893378df74eb0e8be4509d
e-caliano <e.caliano2@gmail.com>
2023-03-14 21:04:02 +0100

    creazione della prima parte dello scheletro della classe "Labirinto"

---
+++ labirinto.py
@@ -0,0 +1,30 @@
+# import numpy as np
+from PIL import Image
+
+
+class Labirinto:
+    def __init__(self):
+        pass
+
+    def parse_input(input_file):
+        """
+        metodo per il parsing dell'input: se il file è .json allora dovrò chiamare una funzione che dal json mi crea una istanza del labirinto,
+        se il file è di tipo immagine allora dovrò chiamare un metodo che crea un'istanza del labirinto a partire dall'immagine.
+        :return: labirinto
+        """
+        pass
+
+    def from_json(self):
+        """
+        metodo per creare istanza del labirinto a partire dal file json di input
+        :return: labirinto
+        """
+        pass
+
+    def from_image(self):
+        """
+        metodo per creare istanza del labirinto a partire dall'immagine di input
+        :return: labirinto
+        """
+        pass
+
