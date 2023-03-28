001da22052361fa7cb396af65e35c7c790006844
Gaetano <gaetanof1707@gmail.com>
2023-03-28 18:41:49 +0200

    Aggiunta file elaborazione_dati.py e file main_progetto.py con rispettivi commenti

---
+++ elaborazione_dati.py
@@ -0,0 +1,33 @@
+from labirinto import Labirinto
+from elaborazionePercorso import RicercaPercorso
+from risultatoLabirinto import Risultati
+
+def risoluzioneLabirinto():
+    """
+    Metodo utile per richiamare le varie classi e gestire gli output
+    :return:
+    """
+    #Chiedo all'utente di inserire il nome del file presente nella cartella indata
+    file=input('Inserisci nome del file con la sua estensione: ')
+    #richiamo le varie funzioni in base ai parametri richiesti
+    labirinto = Labirinto(file)
+    labirinto.gestisci_input()
+    rp = RicercaPercorso(labirinto)
+    #creo una cartella per gli output
+    outputDir = "output/"
+
+    #ottengo la lista dei percorsi per il file scelto e la lista dei costi totali
+    paths, costo_totale = rp.trova_percorsi()
+
+    #per ogni percorso creerò i file dei risultati e li inserisco nella cartella degli output
+    for i, percorsi in enumerate(paths):
+        risultati = Risultati(labirinto, percorsi, costo_totale)
+        index = i
+        #Se il percorso ha costo 'inf' mostro che non è stato trovato nessun percorso
+        if costo_totale[i] == float('inf'):
+            index = i+1
+            print('Non è stato trovato nessun percorso per il percorso numero: {0}'.format(index))
+
+        # crea l'immagine e il file JSON
+        risultati.crea_immagine(outputDir + 'Risultati-{0}.png'.format(i))
+        risultati.crea_file_json(outputDir + 'Risultati-{0}.json'.format(i), index)
---
+++ main_progetto.py
@@ -0,0 +1,18 @@
+from elaborazione_dati import risoluzioneLabirinto
+import os
+
+
+def checkDirectory(nameDirectory):
+    """
+    Metodo che consente di svolgere un check per vedere le directory e, quindi, nel caso non ci sia una directory, viene creata
+    :param nameDirectory:
+    :return:
+    """
+    isExist = os.path.exists(nameDirectory)
+    if not isExist:
+        os.makedirs(nameDirectory)
+
+checkDirectory("output")
+checkDirectory("json_image")
+
+risoluzioneLabirinto()
